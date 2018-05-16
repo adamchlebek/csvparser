34c489e8fef6ce5cddf15cada17495c5cabd4207
Adam Chlebek <adamchlebek@live.com>
2018-05-16 10:42:28 -0400

    Lots of stuff... Added Scripts to start working

--- CSV Parser/Form1.Designer.vb
+++ CSV Parser/Form1.Designer.vb
@@ -27,13 +27,40 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.ListBox5 = New System.Windows.Forms.ListBox()
+        Me.ListBox6 = New System.Windows.Forms.ListBox()
+        Me.ListBox7 = New System.Windows.Forms.ListBox()
+        Me.ListBox8 = New System.Windows.Forms.ListBox()
+        Me.ListBox9 = New System.Windows.Forms.ListBox()
+        Me.ListBox10 = New System.Windows.Forms.ListBox()
+        Me.ListBox11 = New System.Windows.Forms.ListBox()
+        Me.ListBox12 = New System.Windows.Forms.ListBox()
+        Me.ListBox13 = New System.Windows.Forms.ListBox()
+        Me.ListBox14 = New System.Windows.Forms.ListBox()
+        Me.ListBox15 = New System.Windows.Forms.ListBox()
+        Me.ListBox16 = New System.Windows.Forms.ListBox()
+        Me.ListBox17 = New System.Windows.Forms.ListBox()
+        Me.ListBox18 = New System.Windows.Forms.ListBox()
+        Me.ListBox19 = New System.Windows.Forms.ListBox()
+        Me.ListBox20 = New System.Windows.Forms.ListBox()
+        Me.ListBox21 = New System.Windows.Forms.ListBox()
+        Me.ListBox22 = New System.Windows.Forms.ListBox()
+        Me.ListBox23 = New System.Windows.Forms.ListBox()
+        Me.ListBox24 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
         Me.Button1.Location = New System.Drawing.Point(12, 12)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(164, 83)
+        Me.Button1.Size = New System.Drawing.Size(158, 23)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Import CSV"
         Me.Button1.UseVisualStyleBackColor = True
@@ -44,9 +71,9 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(12, 101)
+        Me.Button2.Location = New System.Drawing.Point(176, 12)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(164, 23)
+        Me.Button2.Size = New System.Drawing.Size(76, 23)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Test"
         Me.Button2.UseVisualStyleBackColor = True
@@ -54,7 +81,7 @@ Partial Class Form1
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(12, 130)
+        Me.ListBox1.Location = New System.Drawing.Point(12, 41)
         Me.ListBox1.Name = "ListBox1"
         Me.ListBox1.Size = New System.Drawing.Size(76, 251)
         Me.ListBox1.TabIndex = 3
@@ -62,16 +89,264 @@ Partial Class Form1
         'ListBox2
         '
         Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.Location = New System.Drawing.Point(312, 182)
+        Me.ListBox2.Location = New System.Drawing.Point(94, 41)
         Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(237, 251)
+        Me.ListBox2.Size = New System.Drawing.Size(76, 251)
         Me.ListBox2.TabIndex = 4
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(176, 41)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox3.TabIndex = 5
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Location = New System.Drawing.Point(258, 41)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox4.TabIndex = 6
+        '
+        'ListBox5
+        '
+        Me.ListBox5.FormattingEnabled = True
+        Me.ListBox5.Location = New System.Drawing.Point(338, 41)
+        Me.ListBox5.Name = "ListBox5"
+        Me.ListBox5.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox5.TabIndex = 7
+        '
+        'ListBox6
+        '
+        Me.ListBox6.FormattingEnabled = True
+        Me.ListBox6.Location = New System.Drawing.Point(420, 41)
+        Me.ListBox6.Name = "ListBox6"
+        Me.ListBox6.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox6.TabIndex = 8
+        '
+        'ListBox7
+        '
+        Me.ListBox7.FormattingEnabled = True
+        Me.ListBox7.Location = New System.Drawing.Point(502, 41)
+        Me.ListBox7.Name = "ListBox7"
+        Me.ListBox7.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox7.TabIndex = 9
+        '
+        'ListBox8
+        '
+        Me.ListBox8.FormattingEnabled = True
+        Me.ListBox8.Location = New System.Drawing.Point(584, 41)
+        Me.ListBox8.Name = "ListBox8"
+        Me.ListBox8.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox8.TabIndex = 10
+        '
+        'ListBox9
+        '
+        Me.ListBox9.FormattingEnabled = True
+        Me.ListBox9.Location = New System.Drawing.Point(666, 41)
+        Me.ListBox9.Name = "ListBox9"
+        Me.ListBox9.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox9.TabIndex = 11
+        '
+        'ListBox10
+        '
+        Me.ListBox10.FormattingEnabled = True
+        Me.ListBox10.Location = New System.Drawing.Point(748, 41)
+        Me.ListBox10.Name = "ListBox10"
+        Me.ListBox10.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox10.TabIndex = 12
+        '
+        'ListBox11
+        '
+        Me.ListBox11.FormattingEnabled = True
+        Me.ListBox11.Location = New System.Drawing.Point(830, 41)
+        Me.ListBox11.Name = "ListBox11"
+        Me.ListBox11.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox11.TabIndex = 13
+        '
+        'ListBox12
+        '
+        Me.ListBox12.FormattingEnabled = True
+        Me.ListBox12.Location = New System.Drawing.Point(912, 41)
+        Me.ListBox12.Name = "ListBox12"
+        Me.ListBox12.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox12.TabIndex = 14
+        '
+        'ListBox13
+        '
+        Me.ListBox13.FormattingEnabled = True
+        Me.ListBox13.Location = New System.Drawing.Point(12, 298)
+        Me.ListBox13.Name = "ListBox13"
+        Me.ListBox13.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox13.TabIndex = 26
+        '
+        'ListBox14
+        '
+        Me.ListBox14.FormattingEnabled = True
+        Me.ListBox14.Location = New System.Drawing.Point(94, 298)
+        Me.ListBox14.Name = "ListBox14"
+        Me.ListBox14.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox14.TabIndex = 25
+        '
+        'ListBox15
+        '
+        Me.ListBox15.FormattingEnabled = True
+        Me.ListBox15.Location = New System.Drawing.Point(176, 298)
+        Me.ListBox15.Name = "ListBox15"
+        Me.ListBox15.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox15.TabIndex = 24
+        '
+        'ListBox16
+        '
+        Me.ListBox16.FormattingEnabled = True
+        Me.ListBox16.Location = New System.Drawing.Point(258, 298)
+        Me.ListBox16.Name = "ListBox16"
+        Me.ListBox16.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox16.TabIndex = 23
+        '
+        'ListBox17
+        '
+        Me.ListBox17.FormattingEnabled = True
+        Me.ListBox17.Location = New System.Drawing.Point(340, 298)
+        Me.ListBox17.Name = "ListBox17"
+        Me.ListBox17.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox17.TabIndex = 22
+        '
+        'ListBox18
+        '
+        Me.ListBox18.FormattingEnabled = True
+        Me.ListBox18.Location = New System.Drawing.Point(420, 298)
+        Me.ListBox18.Name = "ListBox18"
+        Me.ListBox18.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox18.TabIndex = 21
+        '
+        'ListBox19
+        '
+        Me.ListBox19.FormattingEnabled = True
+        Me.ListBox19.Location = New System.Drawing.Point(502, 298)
+        Me.ListBox19.Name = "ListBox19"
+        Me.ListBox19.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox19.TabIndex = 20
+        '
+        'ListBox20
+        '
+        Me.ListBox20.FormattingEnabled = True
+        Me.ListBox20.Location = New System.Drawing.Point(584, 298)
+        Me.ListBox20.Name = "ListBox20"
+        Me.ListBox20.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox20.TabIndex = 19
+        '
+        'ListBox21
+        '
+        Me.ListBox21.FormattingEnabled = True
+        Me.ListBox21.Location = New System.Drawing.Point(666, 298)
+        Me.ListBox21.Name = "ListBox21"
+        Me.ListBox21.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox21.TabIndex = 18
+        '
+        'ListBox22
+        '
+        Me.ListBox22.FormattingEnabled = True
+        Me.ListBox22.Location = New System.Drawing.Point(748, 298)
+        Me.ListBox22.Name = "ListBox22"
+        Me.ListBox22.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox22.TabIndex = 17
+        '
+        'ListBox23
+        '
+        Me.ListBox23.FormattingEnabled = True
+        Me.ListBox23.Location = New System.Drawing.Point(830, 298)
+        Me.ListBox23.Name = "ListBox23"
+        Me.ListBox23.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox23.TabIndex = 16
+        '
+        'ListBox24
+        '
+        Me.ListBox24.FormattingEnabled = True
+        Me.ListBox24.Location = New System.Drawing.Point(912, 298)
+        Me.ListBox24.Name = "ListBox24"
+        Me.ListBox24.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox24.TabIndex = 15
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(258, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(76, 23)
+        Me.Button3.TabIndex = 27
+        Me.Button3.Text = "Email"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(338, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(76, 23)
+        Me.Button4.TabIndex = 28
+        Me.Button4.Text = "HAddress"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(420, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(76, 23)
+        Me.Button5.TabIndex = 29
+        Me.Button5.Text = "HCity"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(502, 12)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(76, 23)
+        Me.Button6.TabIndex = 30
+        Me.Button6.Text = "HState"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(584, 12)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(76, 23)
+        Me.Button7.TabIndex = 31
+        Me.Button7.Text = "HZIP"
+        Me.Button7.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(607, 499)
+        Me.ClientSize = New System.Drawing.Size(1011, 561)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox13)
+        Me.Controls.Add(Me.ListBox14)
+        Me.Controls.Add(Me.ListBox15)
+        Me.Controls.Add(Me.ListBox16)
+        Me.Controls.Add(Me.ListBox17)
+        Me.Controls.Add(Me.ListBox18)
+        Me.Controls.Add(Me.ListBox19)
+        Me.Controls.Add(Me.ListBox20)
+        Me.Controls.Add(Me.ListBox21)
+        Me.Controls.Add(Me.ListBox22)
+        Me.Controls.Add(Me.ListBox23)
+        Me.Controls.Add(Me.ListBox24)
+        Me.Controls.Add(Me.ListBox12)
+        Me.Controls.Add(Me.ListBox11)
+        Me.Controls.Add(Me.ListBox10)
+        Me.Controls.Add(Me.ListBox9)
+        Me.Controls.Add(Me.ListBox8)
+        Me.Controls.Add(Me.ListBox7)
+        Me.Controls.Add(Me.ListBox6)
+        Me.Controls.Add(Me.ListBox5)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.ListBox3)
         Me.Controls.Add(Me.ListBox2)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button2)
@@ -87,4 +362,31 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents ListBox5 As ListBox
+    Friend WithEvents ListBox6 As ListBox
+    Friend WithEvents ListBox7 As ListBox
+    Friend WithEvents ListBox8 As ListBox
+    Friend WithEvents ListBox9 As ListBox
+    Friend WithEvents ListBox10 As ListBox
+    Friend WithEvents ListBox11 As ListBox
+    Friend WithEvents ListBox12 As ListBox
+    Friend WithEvents ListBox13 As ListBox
+    Friend WithEvents ListBox14 As ListBox
+    Friend WithEvents ListBox15 As ListBox
+    Friend WithEvents ListBox16 As ListBox
+    Friend WithEvents ListBox17 As ListBox
+    Friend WithEvents ListBox18 As ListBox
+    Friend WithEvents ListBox19 As ListBox
+    Friend WithEvents ListBox20 As ListBox
+    Friend WithEvents ListBox21 As ListBox
+    Friend WithEvents ListBox22 As ListBox
+    Friend WithEvents ListBox23 As ListBox
+    Friend WithEvents ListBox24 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
 End Class
--- CSV Parser/Form1.vb
+++ CSV Parser/Form1.vb
@@ -1,31 +1,37 @@
 ﻿Imports System.IO
 Public Class Form1
+
+    Dim displayName As List(Of String) = New List(Of String)()
+    Dim email As List(Of String) = New List(Of String)()
+    Dim emailAddress2 As List(Of String) = New List(Of String)()
+    Dim emailAddress3 As List(Of String) = New List(Of String)()
+    Dim homePhone As List(Of String) = New List(Of String)()
+    Dim businessPhone As List(Of String) = New List(Of String)()
+    Dim homeFax As List(Of String) = New List(Of String)()
+    Dim pager As List(Of String) = New List(Of String)()
+    Dim mobilePhone As List(Of String) = New List(Of String)()
+    Dim homeStreet As List(Of String) = New List(Of String)()
+    Dim homeCity As List(Of String) = New List(Of String)()
+    Dim homeState As List(Of String) = New List(Of String)()
+    Dim homeZip As List(Of String) = New List(Of String)()
+    Dim busAddress As List(Of String) = New List(Of String)()
+    Dim busCity As List(Of String) = New List(Of String)()
+    Dim busState As List(Of String) = New List(Of String)()
+    Dim busZip As List(Of String) = New List(Of String)()
+    Dim jobTitle As List(Of String) = New List(Of String)()
+    Dim organization As List(Of String) = New List(Of String)()
+    Dim notes As List(Of String) = New List(Of String)()
+    Dim birthday As List(Of String) = New List(Of String)()
+    Dim webPage As List(Of String) = New List(Of String)()
+    Dim webPage2 As List(Of String) = New List(Of String)()
+    Dim categories As List(Of String) = New List(Of String)()
+
+
     Public Sub ReadCSV()
-        Using reader = New StreamReader("U:\test.csv")
-            Dim displayName As List(Of String) = New List(Of String)()
-            Dim email As List(Of String) = New List(Of String)()
-            Dim emailAddress2 As List(Of String) = New List(Of String)()
-            Dim emailAddress3 As List(Of String) = New List(Of String)()
-            Dim homePhone As List(Of String) = New List(Of String)()
-            Dim businessPhone As List(Of String) = New List(Of String)()
-            Dim homeFax As List(Of String) = New List(Of String)()
-            Dim pager As List(Of String) = New List(Of String)()
-            Dim mobilePhone As List(Of String) = New List(Of String)()
-            Dim homeStreet As List(Of String) = New List(Of String)()
-            Dim homeCity As List(Of String) = New List(Of String)()
-            Dim homeState As List(Of String) = New List(Of String)()
-            Dim homeZip As List(Of String) = New List(Of String)()
-            Dim busAddress As List(Of String) = New List(Of String)()
-            Dim busCity As List(Of String) = New List(Of String)()
-            Dim busState As List(Of String) = New List(Of String)()
-            Dim busZip As List(Of String) = New List(Of String)()
-            Dim jobTitle As List(Of String) = New List(Of String)()
-            Dim organization As List(Of String) = New List(Of String)()
-            Dim notes As List(Of String) = New List(Of String)()
-            Dim birthday As List(Of String) = New List(Of String)()
-            Dim webPage As List(Of String) = New List(Of String)()
-            Dim webPage2 As List(Of String) = New List(Of String)()
-            Dim categories As List(Of String) = New List(Of String)()
+        Using reader = New StreamReader("U:\mailinglist.csv")
+
+
+            Dim count As Integer = 0
 
             While Not reader.EndOfStream
                 Dim line = reader.ReadLine()
@@ -56,15 +62,76 @@ Public Class Form1
                 categories.Add(values(23))
             End While
 
+
+
             For i As Integer = 1 To displayName.Count()
-                ListBox1.Items.Add(listA(i - 1))
-                ListBox2.Items.Add(listB(i - 1))
+                ListBox1.Items.Add(displayName(i - 1))
+                ListBox2.Items.Add(email(i - 1))
+                ListBox3.Items.Add(emailAddress2(i - 1))
+                ListBox4.Items.Add(emailAddress3(i - 1))
+                ListBox5.Items.Add(homePhone(i - 1))
+                ListBox6.Items.Add(businessPhone(i - 1))
+                ListBox7.Items.Add(homeFax(i - 1))
+                ListBox8.Items.Add(pager(i - 1))
+                ListBox9.Items.Add(mobilePhone(i - 1))
+                ListBox10.Items.Add(homeStreet(i - 1))
+                ListBox11.Items.Add(homeCity(i - 1))
+                ListBox12.Items.Add(homeState(i - 1))
+                ListBox13.Items.Add(homeZip(i - 1))
+                ListBox14.Items.Add(busAddress(i - 1))
+                ListBox15.Items.Add(busCity(i - 1))
+                ListBox16.Items.Add(busState(i - 1))
+                ListBox17.Items.Add(busZip(i - 1))
+                ListBox18.Items.Add(jobTitle(i - 1))
+                ListBox19.Items.Add(organization(i - 1))
+                ListBox20.Items.Add(notes(i - 1))
+                ListBox21.Items.Add(birthday(i - 1))
+                ListBox22.Items.Add(webPage(i - 1))
+                ListBox23.Items.Add(webPage2(i - 1))
+                ListBox24.Items.Add(categories(i - 1))
+                count += 1
             Next
+
+            Button2.Text = count
+
+            checkNames()
 
         End Using
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ReadCSV()
+    End Sub
+
+    Public Sub checkNames()
+        Dim count As Integer = 0
+
+        For i As Integer = 1 To displayName.Count()
+            If displayName.Item(i - 1).Contains("and") Then
+                count += 1
+            End If
+        Next
+
+        Button2.Text = count
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        emailWrite(email)
+    End Sub
+
+    Public Sub emailWrite(ByVal selectedList As List(Of String))
+        Dim count As Integer = 0
+        Threading.Thread.Sleep(2000)
+        For i As Integer = 1 To selectedList.Count()
+            SendKeys.Send(selectedList.Item(i - 1))
+            SendKeys.Send("{ENTER}")
+        Next
+
+        Button3.Text = count
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ReadCSV()
     End Sub
 End Class
