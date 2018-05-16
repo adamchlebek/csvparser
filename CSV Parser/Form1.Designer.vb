c95f975060b76b4e30a2bad5c7555736f361725c
Adam Chlebek <adamchlebek@live.com>
2018-05-16 10:07:59 -0400

    My computer shut down and i got scared

--- CSV Parser/Form1.Designer.vb
+++ CSV Parser/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,71 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(164, 83)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Import CSV"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 101)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(164, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Test"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 130)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(76, 251)
+        Me.ListBox1.TabIndex = 3
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(312, 182)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(237, 251)
+        Me.ListBox2.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(607, 499)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
 End Class
--- CSV Parser/Form1.vb
+++ CSV Parser/Form1.vb
@@ -1,3 +1,70 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Public Class Form1
+    Public Sub ReadCSV()
+        Using reader = New StreamReader("U:\test.csv")
+            Dim displayName As List(Of String) = New List(Of String)()
+            Dim email As List(Of String) = New List(Of String)()
+            Dim emailAddress2 As List(Of String) = New List(Of String)()
+            Dim emailAddress3 As List(Of String) = New List(Of String)()
+            Dim homePhone As List(Of String) = New List(Of String)()
+            Dim businessPhone As List(Of String) = New List(Of String)()
+            Dim homeFax As List(Of String) = New List(Of String)()
+            Dim pager As List(Of String) = New List(Of String)()
+            Dim mobilePhone As List(Of String) = New List(Of String)()
+            Dim homeStreet As List(Of String) = New List(Of String)()
+            Dim homeCity As List(Of String) = New List(Of String)()
+            Dim homeState As List(Of String) = New List(Of String)()
+            Dim homeZip As List(Of String) = New List(Of String)()
+            Dim busAddress As List(Of String) = New List(Of String)()
+            Dim busCity As List(Of String) = New List(Of String)()
+            Dim busState As List(Of String) = New List(Of String)()
+            Dim busZip As List(Of String) = New List(Of String)()
+            Dim jobTitle As List(Of String) = New List(Of String)()
+            Dim organization As List(Of String) = New List(Of String)()
+            Dim notes As List(Of String) = New List(Of String)()
+            Dim birthday As List(Of String) = New List(Of String)()
+            Dim webPage As List(Of String) = New List(Of String)()
+            Dim webPage2 As List(Of String) = New List(Of String)()
+            Dim categories As List(Of String) = New List(Of String)()
 
+            While Not reader.EndOfStream
+                Dim line = reader.ReadLine()
+                Dim values = line.Split(","c)
+                displayName.Add(values(0))
+                email.Add(values(1))
+                emailAddress2.Add(values(2))
+                emailAddress3.Add(values(3))
+                homePhone.Add(values(4))
+                businessPhone.Add(values(5))
+                homeFax.Add(values(6))
+                pager.Add(values(7))
+                mobilePhone.Add(values(8))
+                homeStreet.Add(values(9))
+                homeCity.Add(values(10))
+                homeState.Add(values(11))
+                homeZip.Add(values(12))
+                busAddress.Add(values(13))
+                busCity.Add(values(14))
+                busState.Add(values(15))
+                busZip.Add(values(16))
+                jobTitle.Add(values(17))
+                organization.Add(values(18))
+                notes.Add(values(19))
+                birthday.Add(values(20))
+                webPage.Add(values(21))
+                webPage2.Add(values(22))
+                categories.Add(values(23))
+            End While
+
+            For i As Integer = 1 To displayName.Count()
+                ListBox1.Items.Add(listA(i - 1))
+                ListBox2.Items.Add(listB(i - 1))
+            Next
+
+        End Using
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ReadCSV()
+    End Sub
 End Class
